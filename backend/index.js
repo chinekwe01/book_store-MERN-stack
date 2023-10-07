47f543053bfd2154ef6aa670ecb5594c8790787c
clever_chinekwe <chinekwe_clever@outlook.com>
2023-10-07 12:40:55 +0100

    added first http route

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(234).send('Welcome to MERN Stack Tuts');
+});
+
 app.listen(PORT, () => {
     console.log(`App is listening on ${PORT}`);
 });
