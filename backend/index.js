4ecef02ab4bfe14171c715eda7f8112c64da6e15
clever_chinekwe <chinekwe_clever@outlook.com>
2023-10-09 12:46:25 +0100

    CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,26 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import {Book} from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
 
+// Middleware for handling CORS POLICY
+
+// option 1: Allow all origins with default of cors(*)
+// app.use(cors());
+
+// option 2: Allow custom origins
+app.use(cors(
+    {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['content-type'],
+    }
+));
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome to MERN Stack Tuts');
