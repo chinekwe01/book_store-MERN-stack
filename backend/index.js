7282d255cccc3968892b435bdf24c071b0ca8b8a
clever_chinekwe <chinekwe_clever@outlook.com>
2023-10-09 07:34:27 +0100

    update a book by id

--- backend/index.js
+++ backend/index.js
@@ -63,6 +63,29 @@ app.get('/books/:id', async (request, response) => {
     }
 });
 
+// Route for update a Book
+app.put('/books/:id', async (request,response) => {
+    try {
+        if(
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ){
+            return response.status(404).send({
+                message: 'Send all required fields: title, author, publisher',
+        });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if(!result) {
+        return response.status(404).json({message: 'Book not found'});
+    }
+    return response.status(200).send({message: 'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
