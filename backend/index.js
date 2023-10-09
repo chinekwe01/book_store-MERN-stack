674fa854b0b0b1e59dd92030497ee33232535103
clever_chinekwe <chinekwe_clever@outlook.com>
2023-10-09 07:22:30 +0100

    get one book by id

--- backend/index.js
+++ backend/index.js
@@ -50,6 +50,20 @@ app.get('/books', async (request, response) => {
         response.status(500).send({message: error.message});
     }
 });
+
+// Route to Get One Book from database
+app.get('/books/:id', async (request, response) => {
+    try {
+        const {id} = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book ); 
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
