7e13829d5bf9f2079bcd55e244566dbe22bda9e6
clever_chinekwe <chinekwe_clever@outlook.com>
2023-10-10 20:05:28 +0100

    show books list in React

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
-import createBooks from './pages/CreateBooks';
-import deleteBook from './pages/DeleteBook';
-import editBook from './pages/EditBook';
-import showBook from './pages/ShowBook';
+import CreateBooks from './pages/CreateBooks';
+import DeleteBook from './pages/DeleteBook';
+import EditBook from './pages/EditBook';
+import ShowBook from './pages/ShowBook';
 
 export const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path='/books/create' element={<createBooks/>} />
-      <Route path='/books/details/:id' element={<showBook/>} />
-      <Route path='/books/edit/:id' element={<editBook/>} />
-      <Route path='/books/delete/:id' element={<deleteBook/>} />
+      <Route path='/books/create' element={<CreateBooks/>} />
+      <Route path='/books/details/:id' element={<ShowBook/>} />
+      <Route path='/books/edit/:id' element={<EditBook/>} />
+      <Route path='/books/delete/:id' element={<DeleteBook/>} />
     </Routes>
   )
 }
---
+++ frontend/src/pages/EditBook.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const EditBook = () => {
+  return (
+    <div>EditBook</div>
+  )
+}
+export default EditBook
